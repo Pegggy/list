207f77ca58ccc2f0b224a80d0c18e3b91728fbf0
penfan <fangpei9212@163.com>
2017-08-30 20:20:24 +0800

    引入 jQuery

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import _ from 'lodash';
+import $ from 'jquery';
 
 function component() {
-  var element = document.createElement('div');
+  var element = $('<div></div>');
   // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.html(_.join(['Hello', 'PenFan'], ' ')) ;
 
-  return element;
+  return element.get(0);
 }
 
 document.body.appendChild(component());
