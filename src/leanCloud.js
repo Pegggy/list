2283dd46e4e73ff57eb826a06ed570cef21b7a2a
penfan <fangpei9212@163.com>
2017-09-09 14:50:50 +0800

    新建 leancloud.js

---
+++ src/leanCloud.js
@@ -0,0 +1,10 @@
+import AV from 'leancloud-storage'
+
+const appId = 'lhnGAvlnkiAuvCfGApyI8vLn-gzGzoHsz'
+const appKey = 'mcnNtPno10tnYn3GJhk7jipk'
+AV.init({ 
+  appId: appId, 
+  appKey: appKey 
+});
+
+export default AV
