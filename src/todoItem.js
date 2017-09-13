cdf645eb3a07dc796c4980cb8b69136445c7b9df
penfan <fangpei9212@163.com>
2017-09-13 14:58:57 +0800

    将TodoItem改写成函数

--- src/todoItem.js
+++ src/todoItem.js
@@ -1,18 +1,21 @@
 import React,{Component} from 'react';
 import './todoitem.css';
-export default class TodoItem extends Component{
-  render(){
-    return <div className="todo-item"> 
-      <input type="checkbox" checked={this.props.todo.status==='completed'} 
-         onChange={this.toggle.bind(this)}/>
-      <span>{this.props.todo.title}</span>
-      <button className="del" onClick={this.delete.bind(this)}>删除</button>
+
+export default function(props){
+  return(
+    <div className="todo-item"> 
+      <input type="checkbox" 
+      checked={props.todo.status==='completed'} 
+      onChange={toggle.bind(null,props)}/>
+      <span>{props.todo.title}</span>
+      <button className="del" 
+      onClick={deleted.bind(null,props)}>删除</button>
     </div>
-  }
-  toggle(e){
-    this.props.onToggle(e,this.props.todo);
-  }
-  delete(e){
-    this.props.onDelete(e,this.props.todo);
-  }
+  )
+}
+function toggle(props,e){
+  props.onToggle(e,props.todo)
+}
+function deleted(props,e){
+  props.onDelete(e,props.todo)
 }
